b9e0d10b76f31e0c20bc251f76a30dd55d59669f
Vadym <vadym@example.com>
2026-09-11 10:38:09 +0300

    Solution3

--- task.sql
+++ task.sql
@@ -8,7 +8,7 @@ START TRANSACTION;
 INSERT INTO Orders (CustomerID, Date)
 	VALUES (1, '2023-01-01');
 INSERT INTO OrderItems (OrderID, ProductID, Count)
-	VALUES (LAST_INSERT_ID(), 1, 1)
+	VALUES (LAST_INSERT_ID(), 1, 1);
 UPDATE Products SET WarehouseAmount = WarehouseAmount - 1 WHERE ID = 1;
 
 COMMIT; 
